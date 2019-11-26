86784b394bfc12f5e7a7c6064f7e49ce1a4f5c4a
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2019-11-26 19:24:13 +0300

    edit

--- Module 4/6/MyCalendar/src/MyBirthdayCalendar.java
+++ Module 4/6/MyCalendar/src/MyBirthdayCalendar.java
@@ -1,6 +1,8 @@
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 public class MyBirthdayCalendar
@@ -11,12 +13,11 @@ public class MyBirthdayCalendar
         Calendar calendar = Calendar.getInstance();
         myCalendar.set(1989, Calendar.JULY, 8);
 
-        SimpleDateFormat df = new SimpleDateFormat("dd MMM yyy - EEEE", new Locale("ru"));
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE", new Locale("ru"));
 
-        for (int i = 0; myCalendar.get(Calendar.YEAR) <= calendar.get(Calendar.YEAR); )
-        {
-            Date myBirthDate = myCalendar.getTime();
-            System.out.println(i + " - " + df.format(myBirthDate));
+        for (int i = 0; myCalendar.get(Calendar.YEAR) <= calendar.get(Calendar.YEAR); ) {
+            LocalDate myBirthDate = LocalDate.of(myCalendar.get(Calendar.YEAR), Month.JULY, 8);
+            System.out.println(i + " - " + formatter.format(myBirthDate.getDayOfWeek()) + " \t\t " + myBirthDate);
             myCalendar.add(Calendar.YEAR, 1);
             i++;
         }
