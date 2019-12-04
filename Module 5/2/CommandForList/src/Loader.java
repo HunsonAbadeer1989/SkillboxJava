9b4ea5a163c07faa3009d09767e71af911ad41e5
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2019-12-04 15:16:34 +0300

    CommandForList was  edited

--- Module 5/2/CommandForList/src/Loader.java
+++ Module 5/2/CommandForList/src/Loader.java
@@ -2,12 +2,9 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
+public class Loader {
+    public static void main(String[] args) {
         boolean endProgram = false; // bool for control while cycle line 19
-
         Scanner scan = new Scanner(System.in);
         List<String> agendaList = new ArrayList<>();
         agendaList.add("Important business!");
@@ -17,82 +14,69 @@ public class Loader
         agendaList.add("Family business!");
 
         while (!endProgram) {
-            Pattern pattern = Pattern.compile("([A-Z]+)\\s?(\\d+|\\w+\\s?\\w*\\s*\\w+)?\\s*(\\w+\\s?\\w*\\s*\\w+)?");
-            Pattern dp = Pattern.compile("\\d+");
+            Pattern commandPattern = Pattern.compile("(?<comm>[A-Z]{3,4})\\s?(?<num>\\d+)?\\s*(?<business>[\\w+\\s+]*)");
+            Pattern digitPattern = Pattern.compile("\\d+");
             System.out.println("Input your command: ");
             String command = scan.nextLine();
-            Matcher m = pattern.matcher(command);
-            m.find();
-
-            // switch check the command to use method
-            try {
-                switch (m.group(1)) {
-                    case ("LIST"):
+            Matcher commandMatcher = commandPattern.matcher(command);
+            if ( commandMatcher.find() ) {
+                switch (commandMatcher.group("comm")) {
+                    case "LIST":
                         printList(agendaList);
                         break;
-
-                    case ("ADD"):
-                        Matcher dm = dp.matcher(m.group(2));
+                    case "ADD":
+                        Matcher dm = digitPattern.matcher(commandMatcher.group("num"));
                         if (dm.find()) {
-                            addBusiness(agendaList, Integer.parseInt(m.group(2)), m.group(3));
+                            addBusiness(agendaList, Integer.parseInt(commandMatcher.group("num")), commandMatcher.group("business"));
                             printList(agendaList);
                         } else {
-                            addBusiness(agendaList, m.group(2));
+                            addBusiness(agendaList, commandMatcher.group("num"));
                             printList(agendaList);
                         }
                         break;
-
-                    case ("DELETE"):
-                        deleteBusiness(agendaList, Integer.parseInt(m.group(2)));
+                    case "DELETE":
+                        deleteBusiness(agendaList, Integer.parseInt(commandMatcher.group("num")));
                         printList(agendaList);
                         break;
-
-                    case ("EDIT"):
-                        editBusiness(agendaList, Integer.parseInt(m.group(2)), m.group(3));
+                    case "EDIT":
+                        editBusiness(agendaList, Integer.parseInt(commandMatcher.group("num")), commandMatcher.group("business"));
                         printList(agendaList);
                         break;
-
-                    case ("END"):
+                    case "END":
                         endProgram = true;
                         break;
-
+                    case " ":
+                        System.out.println("Bad input!");
+                        break;
                     default:
                         System.out.println("Try again!");
                         break;
                 }
-            }
-            catch (RuntimeException e){
-                String s = e.getMessage();
-                System.out.println(s);
-                System.out.println("Use only LIST, ADD, EDIT, AND DELETE commands! ");
+            } else {
+                System.out.println("Bad input!");
             }
         }
     }
-
     // Print list
-    public static void printList(List<String> list) {
+    private static void printList(List<String> list) {
         int business = 0;
         for (String x : list)
             System.out.printf("%d - %s\n", business++, x);
     }
-
     // Add business in list
-    public static void addBusiness(List<String> list, String business) {
+    private static void addBusiness(List<String> list, String business) {
         list.add(business);
     }
-
     // Add business in position
-    public static void addBusiness(List<String> list, int to, String business ) {
+    private static void addBusiness(List<String> list, int to, String business) {
         list.add(to, business);
     }
-
     // Delete business from position
-    public static void deleteBusiness(List<String> list, int number) {
+    private static void deleteBusiness(List<String> list, int number) {
         list.remove(number);
     }
-
     // Edit some business
-    public static void editBusiness(List<String> list, int number, String business) {
+    private static void editBusiness(List<String> list, int number, String business) {
         list.remove(number);
         list.add(number, business);
     }
