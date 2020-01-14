1a697d5d03a799520b066c1e7bbed8eef990d9cb
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2020-01-14 12:51:37 +0300

    Airplanes edited

--- Module 7/8/src/Loader.java
+++ Module 7/8/src/Loader.java
@@ -3,27 +3,26 @@ import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 import java.time.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Loader {
     public static void main(String[] args) {
 
         Airport airport = Airport.getInstance();
         List<Terminal> terminals = airport.getTerminals();
-        List<Flight> flights = new ArrayList<>();
-
-        terminals.forEach(t -> {
-            t.getFlights();
-            flights.addAll(t.getFlights());
-        });
-
         LocalDateTime timePlus2hours = LocalDateTime.now().plusHours(2);
-        flights.stream().
+
+        List<Flight> flights = terminals.stream().
+                flatMap(terminal -> terminal.getFlights().stream()).
                 filter(f -> f.getType() == Flight.Type.DEPARTURE).
-                filter(f -> f.getDate().toInstant()
-                        .atZone(ZoneId.systemDefault())
-                        .toLocalDateTime().isAfter(timePlus2hours)).
+                filter(f -> f.getDate().toInstant().
+                        atZone(ZoneId.systemDefault()).
+                        toLocalDateTime().isAfter(timePlus2hours)).
+                        collect(Collectors.toList());
+
+        flights.
                 forEach(f -> {
-                             System.out.printf("Aircraft %s, will %s in %s \n", f.getAircraft(), f.getType(), f.getDate());
-                });
+            System.out.printf("Aircraft %s, will %s in %s \n", f.getAircraft(), f.getType(), f.getDate());
+        });
     }
 }
