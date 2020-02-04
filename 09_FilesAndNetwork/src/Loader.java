be342cb8662e6a0398ce2ea8f5290744d1b5c2e1
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2020-02-04 11:11:29 +0300

    Edited

--- 09_FilesAndNetwork/src/Loader.java
+++ 09_FilesAndNetwork/src/Loader.java
@@ -4,29 +4,21 @@ import java.nio.file.Files;
 
 public class Loader {
     private static final long  MEGABYTE = 1024L * 1024L;
+
     public static void main(String[] args) {
         File folder = new File("/users/hunsonabadeer/IdeaProjects");
-        System.out.println(folder.isDirectory());
-        long size = 0L;
-        try {
-            size = folderSize(folder);
-        }
-        catch(NullPointerException ex){
-            System.out.println(ex.getMessage());
-        }
-        System.out.printf( "Your directory is: %s bytes", size);
-        System.out.printf( "Your directory is: %s Mb", (double) size / MEGABYTE);
+        long size = folderSize(folder);
+        System.out.printf( "Your directory is: %s bytes or %.4s Mb", size, (double) size / MEGABYTE);
     }
 
     public static long folderSize(File directory){
         long size = 0;
-        for (File file : directory.listFiles()) {
-            if (file.isFile())
-                size += file.length();
-            else
-                size += folderSize(file);
+        try {
+            return Files.walk(directory.toPath()).mapToLong(p -> p.toFile().length()).sum();
+        }
+        catch (IOException ex){
+            ex.getMessage();
         }
         return size;
-//        return Files.walk(directory.toPath()).mapToLong(p -> p.toFile().length() ).sum();
     }
 }
