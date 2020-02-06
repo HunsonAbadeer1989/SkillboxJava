ba8afa28f1be1a4980f30ed7c2545f1e48b75e22
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2020-02-06 12:15:00 +0300

    Edited copyDir

--- 09_FilesAndNetwork/src/CopyDirExample.java
+++ 09_FilesAndNetwork/src/CopyDirExample.java
@@ -1,10 +1,7 @@
+import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -13,29 +10,46 @@ public class CopyDirExample {
     private static final String DST_DIR = "/Users/hunsonabadeer/Desktop/dstFolder";
 
     public static void main(String[] args) {
-        copyDirectory(SRC_DIR, DST_DIR);
+//        copyDirectory(SRC_DIR, DST_DIR);
+        copyEntireDir(SRC_DIR, DST_DIR);
     }
 
     private static void copyDirectory(String src, String dst) {
         Path sourceParentFolder = Paths.get(src);
         Path destinationParentFolder = Paths.get(dst);
         try {
+            if (!Files.exists(destinationParentFolder)) {
+                System.out.println("File not found!");
+            }
             Stream<Path> allFilesPathStream = Files.walk(sourceParentFolder);
             Consumer<? super Path> action = (Consumer<Path>) t -> {
                 try {
                     String destinationPath = t.toString().replaceAll(sourceParentFolder.toString(),
                             destinationParentFolder.toString());
                     Files.copy(t, Paths.get(destinationPath));
-                } catch (FileAlreadyExistsException e) {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
             };
             allFilesPathStream.forEach(action);
-
         } catch (FileAlreadyExistsException e) {
             e.getMessage();
+        }
+        catch (NoSuchFileException ex){
+            ex.getStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyEntireDir(String src, String dst){
+        File srcDir = new File(src);
+        File destDir = new File(dst);
+
+        try {
+            FileUtils.copyDirectory(srcDir, destDir);
         } catch (IOException e) {
             e.printStackTrace();
         }
