8feb0eae0a2766f3cb5517f3d6ad5704e5f5c467
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2020-03-19 12:23:00 +0300

    Edit ImageResizer

--- 11_Multithreading/ImageResizer/src/Main.java
+++ 11_Multithreading/ImageResizer/src/Main.java
@@ -1,15 +1,10 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
-public class Main
-{
-    private static final int COUNT_OF_PROCESSORS = Runtime.getRuntime(). availableProcessors();
+public class Main {
+    private static final int COUNT_OF_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
-    public static void main(String[] args)
-    {
-        String srcFolder = "/users/hunsonabadeer/Desktop/LentaImages";
+    public static void main(String[] args) {
+        String srcFolder = "/users/hunsonabadeer/Desktop/src";
         String dstFolder = "/users/hunsonabadeer/Desktop/dst";
 
         File srcDir = new File(srcFolder);
@@ -18,52 +13,20 @@ public class Main
 
         File[] files = srcDir.listFiles();
 
-        for(int i =0; i < COUNT_OF_PROCESSORS; i++){
-                Thread first = new Thread(() -> {
-                    resizeImage(dstFolder, files);
-                });
-                first.start();
+        ImageResizer[] imageResizers = new ImageResizer[COUNT_OF_PROCESSORS];
+
+        for (int i = 0; i < COUNT_OF_PROCESSORS; i++) {
+            assert files != null;
+            int arrSize = Math.incrementExact(files.length / COUNT_OF_PROCESSORS);
+            File[] tempArr = new File[arrSize];
+            System.arraycopy(files, ((files.length / COUNT_OF_PROCESSORS) * i), tempArr, 0, tempArr.length);
+            imageResizers[i] = new ImageResizer(tempArr, dstFolder, start);
         }
 
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
-    }
-
-    private static void resizeImage(String dstFolder, File[] files) {
-        try
-        {
-            for(File file : files)
-            {
-                BufferedImage image = ImageIO.read(file);
-                if(image == null) {
-                    continue;
-                }
-
-                int newWidth = 300;
-                int newHeight = (int) Math.round(
-                        image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                        newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++)
-                {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
-
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
-            }
+        for (ImageResizer imageResizer : imageResizers) {
+            new Thread(imageResizer).start();
         }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
+
     }
 
 }
