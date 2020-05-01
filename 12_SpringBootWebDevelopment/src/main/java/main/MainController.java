9536991f454a11a26d9d89631086394da1803548
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2020-05-01 22:25:35 +0300

    SpringBoot

---
+++ 12_SpringBootWebDevelopment/src/main/java/main/MainController.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+@RestController
+public class MainController {
+    @RequestMapping("/")
+    public String index(){
+        return DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+                .withLocale(new Locale("ru"))
+                .format(LocalDate.now());
+    }
+}
