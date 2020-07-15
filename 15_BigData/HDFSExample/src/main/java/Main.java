b56cdc68cdf9c074e7048afc6eb50e4cc3f40523
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2020-07-15 18:35:50 +0300

    SparkMain

--- 15_BigData/HDFSExample/src/main/java/Main.java
+++ 15_BigData/HDFSExample/src/main/java/Main.java
@@ -2,47 +2,55 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-import java.io.BufferedWriter;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.URI;
+import java.nio.channels.FileChannel;
 
 public class Main
 {
     private static String symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String HADOOP_CONTAINER_ID = "400537a7128a";
+    private static final String HADOOP_CONTAINER_ID = "2fa07e42c821";
     private static final String HADOOP_PATH = "hdfs://" + HADOOP_CONTAINER_ID + ":8020/";
+    private static final String SOURCE_FILE = "src/main/resources/text.txt";
 
     public static void main(String[] args) throws Exception
     {
-        String fileName = "testFile.txt";
-        String dirName = "test";
-        FileAccess fileAccess = new FileAccess(HADOOP_PATH);
-        fileAccess.create(fileName);
-        fileAccess.append(fileName, "Hello my dear friend!");
-        System.out.println(fileAccess.read(fileName));
-        fileAccess.delete(fileName);
-        System.out.println(fileAccess.list(dirName));
+//        String fileName = "testFile.txt";
+//        String dirName = "test";
+//        FileAccess fileAccess = new FileAccess(HADOOP_PATH);
 
-//        Configuration configuration = new Configuration();
-//        configuration.set("dfs.client.use.datanode.hostname", "true");
-//        System.setProperty("HADOOP_USER_NAME", "root");
+//        fileAccess.create(fileName);
+//        fileAccess.append(fileName, "Hello my dear friend!");
+//        System.out.println(fileAccess.read(fileName));
+//        fileAccess.delete(fileName);
+//        System.out.println(fileAccess.list(dirName));
+
+        Configuration configuration = new Configuration();
+        configuration.set("dfs.client.use.datanode.hostname", "true");
+        System.setProperty("HADOOP_USER_NAME", "root");
 //
-//        FileSystem hdfs = FileSystem.get(
-//            new URI("hdfs://400537a7128a:8020"), configuration
-//        );
+        FileSystem hdfs = FileSystem.get(
+            new URI("hdfs://" + HADOOP_CONTAINER_ID + ":8020"), configuration
+        );
 //
-//        Path file = new Path("hdfs://400537a7128a:8020/test/file.txt");
+        Path file = new Path("hdfs://" + HADOOP_CONTAINER_ID + ":8020/test/file.txt");
 //
-//        if (hdfs.exists(file)) {
-//            hdfs.delete(file, true);
-//        }
+        if (hdfs.exists(file)) {
+            hdfs.delete(file, true);
+        }
 //
-//        OutputStream os = hdfs.create(file);
-//        BufferedWriter br = new BufferedWriter(
-//            new OutputStreamWriter(os, "UTF-8")
-//        );
-//
+        OutputStream os = hdfs.create(file);
+        BufferedWriter br = new BufferedWriter(
+            new OutputStreamWriter(os, "UTF-8")
+        );
+
+        BufferedReader reader = new BufferedReader(new FileReader(SOURCE_FILE));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            br.write(line);
+        }
+
 //        for(int i = 0; i < 10_000_000; i++) {
 //            br.write(getRandomWord() + " ");
 //        }
